e3013d5da72cedd80c1f563db5450d559987f35d
ganjabass911 <32351202+ganjabass911@users.noreply.github.com>
2018-04-22 20:34:31 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-# WSGI приложение должно возвращать документ с MIME-типом text/plain, содержащий все GET параметры, по одному на каждую строку.
+# WSGI приложение должно возвращать документ с MIME-типом text/plain, содержащий все GET 
+# параметры, по одному на каждую строку.
 #
 # Например при запросе  /?a=1&a=2&b=3 приложение должно вернуть такой текст
 #
@@ -6,6 +7,22 @@
 # a=2
 # b=3
 
+import multiprocessing
+
+bind = "0.0.0.0:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+
 def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
+    """Simplest possible application object"""
+    data = 'No queries'
+    query_str = environ['QUERY_STRING']
+    if query_str:
+    	data = query_str.replace('&', '\n')
+    status = '200 OK'
+    response_headers = [
+        ('Content-type','text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    data = bytes(data, 'ascii')
+    return iter([data])
