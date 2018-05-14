4ea0f3655dfb8747ad02a889f7bba40ad357b0fb
ganjabass911 <ganjabass911@gmail.com>
2018-05-14 05:29:46 +0300

    1

--- etc/hello.py
+++ etc/hello.py
@@ -2,17 +2,3 @@ import multiprocessing
 bind = "0.0.0.0:8080"
 workers = multiprocessing.cpu_count() * 2 + 1
 
-def app(environ, start_response):
-    """Simplest possible application object"""
-    data = 'No queries'
-    query_str = environ['QUERY_STRING']
-    if query_str:
-        data = query_str.replace('&', '\n')
-    status = '200 OK'
-    response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
-    start_response(status, response_headers)
-    data = bytes(data, 'utf-8')
-    return iter([data])
--- init.sh
+++ init.sh
@@ -5,8 +5,7 @@ sudo /etc/init.d/nginx restart
 sudo rm -r /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
-
+sudo rm -r /etc/gunicorn.d/gunicorn.conf
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
-gunicorn sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf /etc/gunicorn.d/hello:app
-
+  gunicorn
 sudo /etc/init.d/mysql start
