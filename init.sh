063a39ae1a1613fb5a2965387739a9b3edeecdcb
ganjabass911 <ganjabass911@gmail.com>
2018-05-14 05:32:01 +0300

    1

--- init.sh
+++ init.sh
@@ -5,7 +5,5 @@ sudo /etc/init.d/nginx restart
 sudo rm -r /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
-sudo rm -r /etc/gunicorn.d/gunicorn.conf
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
-  gunicorn
+
 sudo /etc/init.d/mysql start
