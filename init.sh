11db7c5059b02026891dcc0d764a0544daa707e6
ganjabass911 <ganjabass911@gmail.com>
2018-05-14 04:49:41 +0300

    1

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@ sudo rm -rf /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo rm -r /etc/gunicorn.d/hello
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello
+sudo rm -r /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
-sudo rm -r /etc/gunicorn.d/gunicorn
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn
+sudo rm -r /etc/gunicorn.d/test2
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test2
 
-sudo /etc/init.d/gunicorn restart -c  /etc/gunicorn.d/hello.py hello:app
+
 sudo /etc/init.d/mysql start
